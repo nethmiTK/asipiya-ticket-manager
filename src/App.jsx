a37929a36b8ee989f8b90c4bbc555f97a1d70662
nethmiTK <nethmitk33@gmail.com>
2025-05-26 16:54:20 +0530

    developed the tickets page
    install-reacticons

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react'
- 
+import Dashboard from './frontend/admin_panel/dashbord';
+import Tickets from './frontend/admin_panel/tickets';
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     
-     </>
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Dashboard />} />
+        <Route path="/tickets" element={<Tickets />} /> */}
+      </Routes>
+    </Router>
   )
 }
 
