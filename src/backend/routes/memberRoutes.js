9f968101abd1629f5b3308050a94ba16f8b1cbc9
nethmiTK <nethmitk33@gmail.com>
2025-05-26 14:34:19 +0530

    Deleted Unneccesry files

--- src/backend/routes/memberRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { fetchUsers, createUser } from "../controllers/memberController.js";
-
-const router = express.Router();
-
-router.get("/", fetchUsers);
-router.post("/", createUser);
-
-export default router;
