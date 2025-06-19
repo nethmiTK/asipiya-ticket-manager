0b417a8dd212260e0a0aaac247300015440e1806
GKariyawasam <govinditharupama@gmail.com>
2025-06-19 12:06:39 +0530

    Evidence View Page

--- src/backend/index.js
+++ src/backend/index.js
@@ -2286,7 +2286,7 @@ app.post('/api/upload_evidence', upload_evidence.array('evidenceFiles'), async (
   try {
     const values = req.files.map(file => [
       ticketId,
-      file.path.replace(/\\/g, '/'), // ✅ Fix path before storing
+      file.path.replace(/\\/g, '/'),
       description
     ]);
 
