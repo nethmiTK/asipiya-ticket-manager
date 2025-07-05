038e27b9dc73af56a6d75b10f3a8cceccb35f689
nethmiTK <nethmitk33@gmail.com>
2025-07-05 17:46:40 +0530

    Revert "Merge pull request #257 from nethmiTK/nethmi"
    This reverts commit 2970e3e025f54b3fedb5113fb55aa9f00d276724, reversing
    changes made to b3253b1e3a30adc787721276d7f60ab057d0809c.

--- src/backend/routes/ticketStatusUpdateRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { updateTicketStatus } from '../controllers/ticketStatusUpdateController.js';
-
-const router = express.Router();
-
-// Update ticket status
-router.put('/ticket_status/:id', updateTicketStatus);
-
-export default router;
